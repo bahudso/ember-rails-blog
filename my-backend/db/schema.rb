b339b9b504d411dbca3ec8835543aca7e2ae7a5d
Ben Hudson <bahhudson@gmail.com>
2014-06-21 21:54:40 -0400

    Migrade db.

--- my-backend/db/schema.rb
+++ my-backend/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140617061018) do
+ActiveRecord::Schema.define(version: 20140622015141) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
