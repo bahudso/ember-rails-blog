fb8a1020fff22fa9115259c21f1021527dad7c27
Ben Hudson <bahhudson@gmail.com>
2014-06-21 21:57:55 -0400

    Add post controller.

--- my-backend/app/controllers/posts_controller.rb
+++ my-backend/app/controllers/posts_controller.rb
@@ -1,2 +1,37 @@
 class PostsController < ApplicationController
+	respond_to :json
+
+	def index
+		respond_with Post.all
+	end
+
+	def show
+		respond_with Post.find(params[:id])
+	end
+
+	def create
+		@post = Post.new(params[:client])
+		@post.title = params[:post][:title]
+		@post.body = params[:post][:body]
+	    if @post.save
+	      respond_with @post
+	    end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		if @post.present?
+			@post.destroy
+		end
+		respond_with nil
+	end
+
+	def update
+		post = Post.find(params[:id])
+		post.title = params[:post][:title]
+		post.body = params[:post][:body]
+		if post.save
+			respond_with post
+		end
+	end
 end
