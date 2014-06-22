dfacbe4410d6421fac17b4531859994b16a3007c
Ben Hudson <bahhudson@gmail.com>
2014-06-21 22:00:26 -0400

    Add seed data for Posts.

--- my-backend/db/seeds.rb
+++ my-backend/db/seeds.rb
@@ -6,9 +6,9 @@ User.create([
      password: '12345678', password_confirmation: '12345678'}
 ])
 
-Post.create(
+Post.create([
   {title: "A Sample Post",
   body: "This will be a simple post record."},
   {title: "Sample Post 2",
   body: "This is sample post number two."}
-)
+])
