bcc1cc74cb5f1cf0cacae12d61742cad22f19275
Ben Hudson <bahhudson@gmail.com>
2014-07-01 14:25:54 -0400

    Add edit for posts.

--- my-frontend/app/router.js
+++ my-frontend/app/router.js
@@ -8,7 +8,9 @@ Router.map(function() {
 	this.route('application');
 	this.route('login');
 	this.resource('posts', function() {
+		this.route('new');
 		this.route('show', {path: ':post_id'});
+		this.route('edit', {path: '/:post_id/edit'});
 	});
 });
 
